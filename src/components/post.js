82a14cf0d7557c625043a965afe7d035193e1648
Natalia Bystrova <n.a.bystrova@gmail.com>
2020-01-17 13:13:19 +0300

    Added Modal

--- src/components/post.js
+++ src/components/post.js
@@ -6,6 +6,7 @@ function renderPost({ postTemplate, postsContainer }) {
       data.forEach((post) => {
         const { src, description } = post;
         const element = document.createElement('div');
+       
         element.innerHTML = postTemplate.innerHTML;
 
         const image = element.querySelector('[data-field="photo"]');
@@ -14,10 +15,26 @@ function renderPost({ postTemplate, postsContainer }) {
 
         const concreteElement = element.firstElementChild;
 
+        const modalTemplate = document.querySelector('#modal_template');
+        const modalElement = document.createElement('div');
+        modalElement.classList.add('postModal-wrapper')
+        modalElement.innerHTML = modalTemplate.innerHTML;
+
+        const imageModal = modalElement.querySelector('[data-field="photoModal"]');
+        imageModal.src = src;
+
         postsContainer.appendChild(concreteElement);
         concreteElement.addEventListener('click', (event) => {
           console.log(post);
+          document.body.appendChild(modalElement);
+
         });
+
+        const closeButton = modalElement.querySelector('.btnClose');
+        closeButton.addEventListener('click', function(e) {
+          e.preventDefault;
+          document.body.removeChild(modalElement);
+        })
       });
     });
 }
